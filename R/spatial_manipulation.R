83267a337939057e87913709cd5a7bc3a7df9d85
Abson-dev <aboubacarhema94@gmail.com>
2020-03-17 14:47:04 +0000

    Update spatial_manipulation.R

--- R/spatial_manipulation.R
+++ R/spatial_manipulation.R
@@ -34,46 +34,7 @@ names(worlClim)
 plot(worlClim)
 plot(worlClim$wc2.0_bio_30s_17)
 #############################################
-worldRaster <- raster(system.file("external/bioclim/current/bio3.grd", package = "biomod2"))
-worldRaster[!is.na(worldRaster)] <- 0
-plot(worldRaster)
-library(rgdal)
-e<-extent(334191.9, 354055.6, 1598513, 1618456)
-shp2raster <- function(shp, mask.raster, label, value, transform = FALSE, proj.from = NA,
-                       proj.to = NA, map = TRUE) {
-  require(raster, rgdal)
-  
-  # use transform==TRUE if the polygon is not in the same coordinate system as
-  # the output raster, setting proj.from & proj.to to the appropriate
-  # projections
-  if (transform == TRUE) {
-    proj4string(shp) <- proj.from
-    shp <- spTransform(shp, proj.to)
-  }
-  
-  # convert the shapefile to a raster based on a standardised background
-  # raster
-  r <- rasterize(shp, mask.raster)
-  # set the cells associated with the shapfile to the specified value
-  r[!is.na(r)] <- value
-  # merge the new raster with the mask raster and export to the working
-  # directory as a tif file
-  r <- mask(merge(r, mask.raster), mask.raster, filename = label, format = "GTiff",
-            overwrite = T)
-  
-  # plot map of new raster
-  if (map == TRUE) {
-    plot(r, main = label, axes = F, box = F)
-  }
-  
-  names(r) <- label
-  return(r)
-}
-####
-# extract all Australian polygons and convert to a world raster where cells
-# associated with Australia have a value of 1 and everything else has a
-# value of 0.
-shp2raster(shp = zone_etude ,mask.raster = worldRaster, label = "Where Amy currently lives", transform = FALSE, value = 1)
+
 #######################################
 filename_zone<-paste0("C:\\Users\\Hp\\OneDrive\\Memoire_ITS4","\\Diohine_Echanti_Classif.shp")
 filename_zone
