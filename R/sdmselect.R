2b58122aa70ec2b8941d5862012008fc3c7556ad
Abson-dev <aboubacarhema94@gmail.com>
2020-03-20 14:51:49 +0000

    Update sdmselect.R

--- R/sdmselect.R
+++ R/sdmselect.R
@@ -21,20 +21,29 @@ worldClim.crop<-crop(worlClim,ext)
 #data<-CovarExtract(x=Base_Espece,cov.paths = l1)
 library(sf)
 data_df<-st_drop_geometry(Base_Espece)
-data_Faidherbia<-data_df %>%
-  filter(Faidherbia_albida==1)
-Faidherbia<-data_Faidherbia %>%
-  select(xcoord,ycoord)
+class(data_df)
+# data_Faidherbia<-data_df %>%
+#   filter(Faidherbia_albida==1)
+# Faidherbia<-data_Faidherbia %>%
+#   select(xcoord,ycoord)
 
 # data.prepared<-Prepare_dataset(x=data, var = 1,cov =covar, datatype = "PA", na.rm = TRUE )
-plot(worldClim.crop,1)
-points(Faidherbia,col='blue')
+# plot(worldClim.crop,1)
+# points(Faidherbia,col='blue')
 #############zones
-data_Faidherbia_1<-data_Faidherbia %>%
-  filter(Zone==1)
-Faidherbia_1<-data_Faidherbia_1 %>%
-  select(xcoord,ycoord)
-plot(worldClim.crop,1)
-plot(zone_etude, add=TRUE)
-points(Faidherbia_1,col='blue')
-
+# data_Faidherbia_1<-data_Faidherbia %>%
+#   filter(Zone==1)
+# Faidherbia_1<-data_Faidherbia_1 %>%
+#   select(xcoord,ycoord)
+# plot(worldClim.crop,1)
+# plot(zone_etude, add=TRUE)
+# points(Faidherbia_1,col='blue')
+#######################################
+Base_Espece_Zone1<-data_sf %>%
+  filter(Zone == 1)
+Base_Espece_Zone2<-data_sf %>%
+  filter(Zone == 2)
+Base_Espece_Zone4<-data_sf %>%
+  filter(Zone == 3)
+Base_Espece_Zone1<-data_sf %>%
+  filter(Zone == 4)
