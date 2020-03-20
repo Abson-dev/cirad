121eb6c869bc504ed7e171d5498828af78d8e664
Abson-dev <aboubacarhema94@gmail.com>
2020-03-20 12:52:28 +0000

    Update sdmselect.R

--- R/sdmselect.R
+++ R/sdmselect.R
@@ -1,1 +1,38 @@
 library(SDMSelect)
+library(dplyr)
+Base_Espece<-Species
+Base_Espece$Faidherbia_albida<-if_else(Base_Espece$Species =="Faidherbia albida",1,0)
+#Balanites aegyptiaca
+Base_Espece$Balanites_aegyptiaca<-if_else(Base_Espece$Species =="Balanites aegyptiaca",1,0)
+#Anogeissus leiocarpus
+Base_Espece$Anogeissus_leiocarpus<-if_else(Base_Espece$Species =="Anogeissus leiocarpus",1,0)
+#Adansonia digitata
+Base_Espece$Adansonia_digitata<-if_else(Base_Espece$Species =="Adansonia digitata",1,0)
+#Acacia nilotica
+Base_Espece$Acacia_nilotica<-if_else(Base_Espece$Species =="Acacia nilotica",1,0)
+##############
+l1<-list.files("D:\\Stage_SDM\\SDM\\Data\\WorldClim\\wc2.0_30s_bio\\",patt="\\.tif")
+l1<-sprintf("D:\\Stage_SDM\\SDM\\Data\\WorldClim\\wc2.0_30s_bio\\%s",l1)
+worlClim<-stack(l1)
+covar<-names(worlClim)
+ext<-extent(Species)
+worldClim.crop<-crop(worlClim,ext)
+
+#data<-CovarExtract(x=Base_Espece,cov.paths = l1)
+library(sf)
+data_df<-st_drop_geometry(Base_Espece)
+data_Faidherbia<-data_df %>%
+  filter(Faidherbia_albida==1)
+Faidherbia<-data_Faidherbia %>%
+  select(xcoord,ycoord)
+
+# data.prepared<-Prepare_dataset(x=data, var = 1,cov =covar, datatype = "PA", na.rm = TRUE )
+plot(worldClim.crop,1)
+points(Faidherbia,col='blue')
+#############zones
+data_Faidherbia_1<-data_Faidherbia %>%
+  filter(Zone==1)
+Faidherbia_1<-data_Faidherbia_1 %>%
+  select(xcoord,ycoord)
+plot(worldClim.crop,1)
+points(Faidherbia_1,col='blue')
